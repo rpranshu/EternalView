7df47b80f000d4f79eb5a4f7d86d6c0135f00404
Pranshu Ranakoti <16818486+rpranshu@users.noreply.github.com>
2018-01-28 09:19:35 +0000

    Add files via upload

---
+++ eternalview.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from sys import platform
+from termcolor import colored
+from urllib2 import *
+
+if platform == "linux" or platform == "linux2" or platform == "darwin":
+	os.system("resize -s 27 85")
+elif platform == "win32":
+	os.system("mode con cols=85 lines=27")
+print colored('                           -= PRANSHU RANAKOTI  ©2018 =-                             ','red',attrs=['bold'])
+print ' '
+def eternal():
+	print colored('   ▄████████     ███        ▄████████    ▄████████ ███▄▄▄▄      ▄████████   ▄█      ' , 'red')
+	print colored('  ███    ███ ▀█████████▄   ███    ███   ███    ███ ███▀▀▀██▄   ███    ███  ███      ' , 'red')
+	print colored('  ███    █▀     ▀███▀▀██   ███    █▀    ███    ███ ███   ███   ███    ███  ███      ' , 'red')
+	print colored(' ▄███▄▄▄         ███   ▀  ▄███▄▄▄      ▄███▄▄▄▄██▀ ███   ███   ███    ███  ███      ' , 'red')
+	print colored('▀▀███▀▀▀         ███     ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███   ███ ▀███████████  ███      ' , 'red')
+	print colored('  ███    █▄      ███       ███    █▄  ▀███████████ ███   ███   ███    ███  ███      ' , 'red')
+	print colored('  ███    ███     ███       ███    ███   ███    ███ ███   ███   ███    ███  ███▌    ▄' , 'red')
+	print colored('  ██████████    ▄████▀     ██████████   ███    ███  ▀█   █▀    ███    █▀   █████▄▄██' , 'red')
+
+eternal()
+print ' '
+print colored('Welcome to EternalView, the all-seeing ᒡ◯ᵔ◯ᒢ information gathering tool!', 'cyan',)
+print ' ' 
+def opt():
+    print ' ***********',
+    print colored ('MENU','yellow',attrs=['bold']),
+    print '***********'
+    print ' *                          *'
+    print colored(" * 1. Whois information     * ",'white',attrs=['bold'])
+    print " * 2. DNS Lookup            * "
+    print colored(" * 3. Cloudflare Detection  * ",'cyan', attrs=['bold'])
+    print colored(" * 4. IP Locator            * ",'magenta',attrs=['bold'])
+    print colored(" * 5. HTTP Headers          * ",'yellow',attrs=['bold'])
+    print " * 6. Robots.txt Scanner    * "
+    print colored(" * 7. Associated links      * ",attrs=['bold'])
+    print colored(" * 8. Nmap Scan             * ",'red',attrs=['bold'])
+    print colored(" * 9. Trace the route       * ",'white',attrs=['bold'])
+    print ' *                          *'
+    print ' ****************************'
+	#print "10. Attacks"
+opt()
+def view():
+	option = input (' '+'\033[0;41m What would you like to do?:\033[1;m ')
+	print ' '
+        if option == 1:
+            ipaddr = raw_input('\033[1;91mDomain address or ip address: \033[1;m')
+            if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
+                whois = "whois"+' '+ipaddr
+                evalwho = os.system(whois)
+                print (evalwho)
+            elif platform == 'win32':
+                wwhois = "http://api.hackertarget.com/whois/?q=" + ipaddr
+                evalwwhois = urlopen(wwhois).read()
+                print (evalwwhois)
+            opt()
+            view()
+        if option == 2:
+            site = raw_input('\033[1;91mEnter Domain: \033[1;m') 
+            ns = "dig" +" "+ site
+            dnseval = os.system(ns)
+            print (dnseval)
+            opt()
+            view()
+        if option ==3:
+        	site = raw_input('\033[1;91mEnter Domain: \033[1;m') 
+        	dns = "http://tools.bevhost.com/cgi-bin/dnslookup?data=" + site
+        	dnseval = urlopen(dns).read()
+        	if 'cloudflare' in dnseval:
+        		print colored("Cloudflare Detected","green",attrs=['bold'])
+        	else:
+        		print colored("Not Protected By cloudflare", "red")
+        	opt()
+        	view()
+        if option == 4:
+         	ipaddr = raw_input('\033[1;91mEnter IP Address: \033[1;m')
+         	loc = 'curl ipinfo.io/'+ipaddr + '/geo'
+         	geoloc = os.system(loc)
+         	print (geoloc)
+         	opt()
+         	view()
+        if option == 5:
+            ipaddr = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+            ipheader = 'curl -v '+' '+ipaddr
+            headereval = os.system(ipheader)
+            print (headereval)
+            opt()
+            view()
+        if option == 6:
+            ipaddr = raw_input('\033[1;91mEnter Domain: \033[1;m')
+            if 'http://' in ipaddr or 'https://' in ipaddr:
+                pass
+            else:
+                ipaddr = 'http://' + ipaddr
+            crawl = ipaddr + "/robots.txt"
+            whocrawl = urlopen(crawl).read()
+            print (whocrawl)
+            opt()
+            view()
+        if option == 7:
+            links = raw_input('\033[1;91mEnter URL: \033[1;m')
+            if 'http://' in links or 'https://' in links:
+                pass
+            else:
+                links = 'http://' + links
+            associated = "https://api.hackertarget.com/pagelinks/?q=" + links
+            evalassociated = urlopen(associated).read()
+            print (evalassociated)
+            opt()
+            view()
+        if option == 8:
+        	ipaddr = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+        	nmap = 'sudo nmap -O -Pn -p 1-65535 -sS -sV'+ ' ' + ipaddr
+        	evalnmap = os.system(nmap)
+        	print (evalnmap)
+        	opt()
+        	view()
+        if option == 9:
+            ipaddr = raw_input('\033[1;91mEnter Domain or IP Address: \033[1;m')
+            track = "traceroute" +' '+ ipaddr
+            trackeval = os.system(track)
+            print (trackeval)
+            opt()
+            view()
+        else:
+        	print colored(' '+'██████INVALID OPTION!!██████','red','on_white',attrs=['blink'])
+        	opt()
+        	view()
+view()
